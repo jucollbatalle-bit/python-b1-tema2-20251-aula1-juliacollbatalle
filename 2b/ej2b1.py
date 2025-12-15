17e00ff0e45d1f3e07e2d9dd34ccab0271052bbb
jucollbatalle-bit <jucollbatalle@uoc.edu>
2025-12-15 21:03:15 +0100

    ej2b1

--- 2b/ej2b1.py
+++ 2b/ej2b1.py
@@ -49,16 +49,10 @@ Exemple:
 """
 
 
-def sum_list_numbers(
-    list_numbers):
-    # Write here your code
-    pass
+def sum_list_numbers(list_numbers):
+    final = 0
+    for number in list_numbers:
+        final += number
+    return final
 
-
-# Si quieres probar tu código, descomenta las siguientes líneas y
-# ejecuta el script
-
-# Si vols provar el teu codi, descomenta les línies següents i executa
-# l'script
-
-# print(sum_list_numbers([50, 10.5, 21, 37.2, 99.9, 40.75, 80]))
+print(sum_list_numbers([50, 10.5, 21, 37.2, 99.9, 40.75, 80]))
